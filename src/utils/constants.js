5ce26dc11584ba7d5202e1bab8391e3e4f9e8135
manveerKaurB <manveerKaur1664@gmail.com>
2024-04-03 20:16:35 +0530

    youtube

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,2 @@
-export const REACT_APP_GOOGLE_API_KEY= "AIzaSyCv6y3PLvqU85YwcG7VH_AqplzHbVLC0ck";
-export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=contentDetails&part=statistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + REACT_APP_GOOGLE_API_KEY;
+export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=contentDetails&part=statistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + process.env.REACT_APP_GOOGLE_API_KEY;
 export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
